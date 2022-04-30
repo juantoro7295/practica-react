3915f60b3e268e0806c56570c113e0425a8bbf34
juantoro7295 <juanpatoro2011@gmail.com>
2022-04-30 16:52:09 -0500

    Cambio de colores
    Se cambiaron color del fondo de varios tem

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ function TodoSearch() {
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Buscar"
       value={searchValue}
       onChange={onSearchValueChange}
     />
